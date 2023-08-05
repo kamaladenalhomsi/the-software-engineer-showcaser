ecdeab3b7c561d2ad24fdc8b19cb1603ac676595
kamaladenalhomsi <kamalalhomsi2015@gmail.com>
2023-08-05 14:05:59 +0000

    Deploying to gh-pages from @ kamaladenalhomsi/the-software-engineer-showcaser@ae953e61bfc66b4d5ee23e4794b44fa7edcab3bd 🚀

---
+++ assets/entries/pages_index_index.page.248bd857.js
@@ -0,0 +1,1 @@
+import{r,a as e,F as t}from"../chunks/chunk-0cbd44a4.js";function o(){return r.useEffect(()=>{window.location.href="/resume"},[]),e(t,{})}export{o as Page};
--- assets/entries/pages_index_index.page.bcef8299.js
+++
@@ -1,1 +0,0 @@
-import{r,j as e,F as t}from"../chunks/chunk-ad3e06ed.js";function a(){return r.useEffect(()=>{window.location.href="/resume"},[]),e(t,{})}export{a as Page};
