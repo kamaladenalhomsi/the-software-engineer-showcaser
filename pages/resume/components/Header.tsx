11e6b0735cfa945e1eb33f98e84151a5c72e200e
kamaladenalhomsi <kamalalhoms2015@gmail.com>
2024-03-30 02:29:57 +0200

    feat: added phone number

--- pages/resume/components/Header.tsx
+++ pages/resume/components/Header.tsx
@@ -7,21 +7,31 @@ type HeaderProps = { basicInfo: Omit<BasicInfo, 'profiles' | 'email'> };
 function Header({ basicInfo }: HeaderProps) {
   return (
     <>
-      <header className="grid items-center justify-between grid-cols-1 sm:grid-cols-6 sm:grid-rows-1 print:grid-cols-6 print:grid-rows-1">
-        {/* <div
-          className="order-3 mt-4 text-lg justify-self-center sm:order-none sm:col-start-1 sm:col-end-2 sm:mt-auto sm:justify-self-start sm:text-sm print:order-none print:col-start-1 print:col-end-2 print:mt-auto print:justify-self-start print:text-sm"
+      <header className="grid grid-cols-1 sm:grid-rows-1 grid-rows-2 items-center justify-between sm:grid-cols-6 sm:grid-rows-1 print:grid-cols-6 print:grid-rows-1 items-center">
+        <div
+          className="order-3 mt-4 text-center justify-center items-center text-lg
+        sm:order-none sm:col-start-1 sm:col-end-2 sm:mt-auto sm:justify-self-start sm:text-sm
+        print:order-none print:col-start-1 print:col-end-2 print:mt-auto print:justify-self-start print:text-sm"
         >
           <Address>
-            {basicInfo.phone} <br /> {basicInfo.address}
+            {basicInfo.phone} <br />
           </Address>
-        </div> */}
-        <div className="justify-center order-2 mt-6 sm:justify-self-start sm:order-none sm:col-start-1 sm:col-end-6 sm:mt-auto print:justify-self-start print:order-none print:col-start-1 print:col-end-6 print:mt-auto">
+        </div>
+        <div
+          className="order-2 mt-6 justify-self-center text-center
+        sm:order-none sm:col-start-2 sm:col-end-6 sm:mt-auto
+        print:order-none print:col-start-2 print:col-end-6 print:mt-auto"
+        >
           <h1 className="text-6xl tracking-tighter text-center title sm:text-left print:text-left">
             <NameKerned kernedLetters={basicInfo.nameKerned} />
           </h1>
         </div>
 
-        <div className="order-1 justify-self-center sm:relative sm:order-none sm:justify-self-auto print:relative print:order-none print:justify-self-auto">
+        <div
+          className="order-1 justify-self-center
+        sm:relative sm:order-none sm:justify-self-auto
+        print:relative print:order-none print:justify-self-auto"
+        >
           <div className="sm:absolute sm:-top-12 sm:right-4 print:absolute print:-top-12 print:right-4">
             <ProfilePicture image={basicInfo.image} />
           </div>
@@ -47,7 +57,7 @@ function NameKerned({ kernedLetters }: NameKerned) {
 
 type AddressProps = { children: React.ReactNode };
 function Address({ children }: AddressProps) {
-  return <address className="not-italic leading-5">{children}</address>;
+  return <address className="not-italic leading-5 text-lg">{children}</address>;
 }
 
 type ProfilePictureProps = { image: string };
